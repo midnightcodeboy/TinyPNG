49a8b2e32d1e319d7b147c7a290ef89fae0f06c3
midnightcodeboy <jess.dahlberg@gmail.com>
2013-06-17 14:42:55 -0300

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd /usr/local/src/;
+wget http://prdownloads.sourceforge.net/optipng/optipng-0.7.4.tar.gz;
+tar -xvzf optipng-0.7.4.tar.gz; 
+cd optipng-0.7.4;
+./configure ;
+make;
+make install;
+cd /usr/local/src/;
+wget http://prdownloads.sourceforge.net/advancemame/advancecomp-1.17.tar.gz?download;
+tar -xvzf advancecomp-1.17.tar.gz ;
+cd advancecomp-1.17;
+./configure;
+make;
+make install;
+cd /usr/local/src/;
+wget http://static.jonof.id.au/dl/kenutils/pngout-20130221-linux.tar.gz;
+tar -xvzf pngout-20130221-linux.tar.gz; 
+cd pngout-20130221-linux;
+cd i386/;
+cp pngout /usr/bin/;
+cd /usr/local/src/;
+wget http://sourceforge.net/projects/pngnq/files/latest/download?source=files;
+tar -xvzf pngnq-1.1.tar.gz; 
+cd pngnq-1.1;
+./configure;
+make;
+make install;
+cd /usr/local/src/;
+wget http://sourceforge.net/projects/pmt/files/pngcrush/1.7.63/pngcrush-1.7.63.tar.gz/download;
+tar -xvzf pngcrush-1.7.63.tar.gz; 
+cd pngcrush-1.7.63;
+./configure;
+make;
+cp pngcrush /usr/bin/;
